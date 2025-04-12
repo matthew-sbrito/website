8a9a73b8e9b8fab666008641b82b6a6325eb7d1d
Matheus Brito dos Santos <matheusbr032@gmail.com>
2025-04-12 11:53:21 -0300

    fix: toggle to await params

--- src/app/[lang]/cv/page.tsx
+++ src/app/[lang]/cv/page.tsx
@@ -4,7 +4,8 @@ import { DictionaryPageProps, getDictionary } from '@/dictionaries';
 
 type Props = DictionaryPageProps;
 
-export default async function CV({ params: { lang } }: Props) {
+export default async function CV({ params }: Props) {
+  const { lang } = await params;
   const dictionary = await getDictionary(lang);
   return redirect(dictionary.resume.redirectLink);
 }
